3f2b91365c54eb316c394c6b7fecd9f7dceabb19
Rutrus <rutrus@gmail.com>
2018-07-12 15:08:10 +0200

    Added first exercises from laptop drive

--- 025.py
+++ 025.py
@@ -1,47 +1,26 @@
 #!/usr/bin/python3
-# https://projecteuler.net
+# https://projecteuler.net/
 # @author Lorenzo Moreno
-
-# @state unresolved; heavy computation
-
-def fibo0(n, a = 0, b = 1):
-    if n == 1: return b
-    return fibo(n-1, b, a+b)
+# @license GPLv3
+#
 
 def fibo(n):
-    a = 0
-    b = 1
-    c = 0
-    for i in range(n-1):
+    a=b=1
+    while n > 2:
         c = a+b
         a = b
         b = c
-    #print(c)
-    return c
+        n -= 1
+    return b
 
-def solve(limit = 1e1000):
-    count = 0
-    i = 0
-    while count < limit:
-        count = fibo(i)
-        i += 1000000
-    return i
+def solve(n):
 
-def solve2():
-    e = fibo(1000)/fibo(1001)
-    counter = 0
-    res = 1e1000
-    while res >1:
-        res = res/e
-        counter += 1
-    print(counter)
-
-from math import e
-
+    for i in range(n,0,-1):
+        digits = len(str(fibo(i)))
+        #print(i,digits)
+        if digits < 1000: return i+1
 
 
 if __name__ == "__main__":
-    assert fibo(12) == 144, "Fibo";
-    #print(e)
-    print(len(str(fibo(int(1e10)))))
-    #print(len(str(fibo(1000))))
+    #assert solve() == None, "First example";
+    print((solve(5000))) #4782
