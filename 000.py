0d6d31bfccdcea2c325d05f07205ecbdff160018
Rutrus <rutrus@gmail.com>
2020-05-18 02:35:29 +0200

    Improved with a pythonic way

--- 000.py
+++ 000.py
@@ -8,5 +8,5 @@ def solve():
     pass
 
 if __name__ == "__main__":
-    assert solve() == None, "First example";
+    assert solve() == None, "First example"
     print(solve())
--- 001.py
+++ 001.py
@@ -4,13 +4,18 @@
 # @license GPLv3
 #
 
-import functools
-def solve(n):
+def solve2(n):
+    """Practising with filter and reduce. Not pythonic."""
+
+    import functools
     return functools.reduce(
         lambda x, y: x+y,
         filter(lambda x: x%3 == 0 or x%5 == 0,range(n))
         )
 
+def solve(n):
+    return sum([i for i in range(n) if i%3 == 0 or i%5 ==0])
+
 if __name__ == "__main__":
-    assert solve(10) == 23, "First example";
+    assert solve(10) == 23, "First example"
     print(solve(1000)) #233168
